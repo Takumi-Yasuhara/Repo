d61136c592ce5689d9f5fbc6f81704fd351811ed
TakumiYasuhara <takumi2.71828@gmail.com>
2025-11-27 15:39:19 +0900

    後期自由制作の英単語ヌメロンです

---
+++ 後期自由制作/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace English_NumerOn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.JPButton = new System.Windows.Forms.Button();
+            this.labelMode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MV Boli", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(24, 357);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(660, 234);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(703, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "単語確定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(915, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(396, 562);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MV Boli", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(24, 47);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(660, 234);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(703, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 86);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "回答！";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("HG創英角ﾎﾟｯﾌﾟ体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(911, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "入力履歴です";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonRandom.Location = new System.Drawing.Point(703, 420);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(180, 73);
+            this.buttonRandom.TabIndex = 6;
+            this.buttonRandom.Text = "ランダム";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(703, 306);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 86);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "諦め～";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // JPButton
+            // 
+            this.JPButton.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.JPButton.Location = new System.Drawing.Point(12, 577);
+            this.JPButton.Name = "JPButton";
+            this.JPButton.Size = new System.Drawing.Size(145, 42);
+            this.JPButton.TabIndex = 8;
+            this.JPButton.Text = "モード切替";
+            this.JPButton.UseVisualStyleBackColor = true;
+            this.JPButton.Click += new System.EventHandler(this.buttonModeChange_Click);
+            // 
+            // labelMode
+            // 
+            this.labelMode.AutoSize = true;
+            this.labelMode.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelMode.Location = new System.Drawing.Point(22, 11);
+            this.labelMode.Name = "labelMode";
+            this.labelMode.Size = new System.Drawing.Size(82, 24);
+            this.labelMode.TabIndex = 9;
+            this.labelMode.Text = "label2";
+            this.labelMode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1316, 631);
+            this.Controls.Add(this.labelMode);
+            this.Controls.Add(this.JPButton);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRandom;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button JPButton;
+        private System.Windows.Forms.Label labelMode;
+    }
+}
+
---
+++ 後期自由制作/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace English_NumerOn
+{
+    public partial class Form1 : Form
+    {
+
+        char[] TargetWords = new char[5];
+        char[] GuessWords = new char[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.MaxLength = 5; // 入力制限
+            textBox2.MaxLength = 5; // 入力制限
+          
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            MessageBox.Show("好きな5文字の英単語を左下に入力してね");
+            MessageBox.Show("右のボタンを押して確定してね");
+            labelMode.Text = "現在";
+           
+
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text.Trim().ToLower();
+            if (input.Length != textBox1.MaxLength)
+            {
+                MessageBox.Show("5文字の英単語を入力してください");
+                
+                return;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                TargetWords[i] = input[i];
+            }
+            listBox1.Items.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+
+            //ResetTextBoxState(textBox1);
+            MessageBox.Show("単語が入力されました！\r\n回答者に交代してね");
+            MessageBox.Show("左上に英単語を入力してね！");
+            MessageBox.Show("右のボタンで回答してね！");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string guess = textBox2.Text.Trim().ToLower();
+
+            if (guess.Length != textBox2.MaxLength)
+            {
+                MessageBox.Show("5文字の英単語を入力してください");
+
+                return;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                GuessWords[i] = guess[i];
+            }
+
+            int hit = 0, blow = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (GuessWords[i] == TargetWords[i]) hit++;
+                else if (TargetWords.Contains(GuessWords[i])) blow++;
+            }
+
+            listBox1.Items.Add($"{guess.ToUpper()} → Hit: {hit}, Blow: {blow}");
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+
+            if (hit == textBox1.MaxLength)
+            {
+                MessageBox.Show("おみごと！クリアです！");
+            }
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            string selected = _WordList[rand.Next(_WordList.Count)];
+            for (int i = 0; i < 5; i++)
+            {
+                TargetWords[i] = selected[i];
+            }
+            listBox1.Items.Clear();
+            textBox1.Clear();
+            //textBox2.Enabled = true;
+            //button2.Enabled = true;
+            MessageBox.Show($"ランダム単語が選ばれました！\r\n回答者に交代してね");
+            MessageBox.Show("左上に英単語を入力してね！");
+            MessageBox.Show("右のボタンで回答してね！");
+        }
+
+        List<string> _WordList = new List<string>
+        {
+              "apple", "grape", "lemon", "mango", "peach",
+              "bread", "chair", "dream", "eagle", "flame",
+              "about","above","actor","adult","after",
+              "again","agree","allow","alone","among",
+              "alter","angel","apart","april","array",
+              "arrow","asset","audio","avoid","awake",
+              "aware","basic","batch","beach","beast",
+              "below","berry","birth","black","blade",
+              "blame","blank","blend","block","blood",
+              "board","booth","bound","brain","brand",
+              //"","","","","", 
+              //"","","","","", 
+              //"","","","","", --単語リスト追加用です
+
+        };
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string answer = new string(TargetWords);
+
+            // 答えを表示
+            MessageBox.Show($"「{answer.ToUpper()}」\r\n知らないの？");
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+        private void buttonModeChange_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                e.SuppressKeyPress = true; // Ctrl+V を無効化
+            }
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                e.SuppressKeyPress = true;
+            }
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                button2.PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
